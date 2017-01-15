2a3c070aa3595345d3ffd0afaf8f3c6828ee4cdb
Laurent MATHOU <lmathou@e3r7p8.42.fr>
2017-01-15 19:07:45 +0100

    modif de munu.php

--- src/menu.php
+++ src/menu.php
@@ -1,16 +1,26 @@
 
-<?PHP
+<?php
+if ($_SESSION['panier']) {
+	foreach ($_SESSION['panier'] as $key => $value)
+		$nb_items++;
+	}
+
 // echo "affichage du " . $_SESSION['logged_as'];
 	if (empty($_SESSION['logged_as'])) {
 	echo "
-		<a href=\"http://localhost:8080/Rush00/src/panier.php\" style=\"color:white;\"><div id=bloc_menu><h3 style=\"margin-top:25px;\">MON PANIER</h3></div></a>
+		<a href=\"http://localhost:8080/Rush00/src/panier.php\" style=\"color:white;\"><div id=bloc_menu><h3 style=\"margin-top:25px;\">MON PANIER";
+	if ($nb_items)
+		echo "<span class='panier_items'> ($nb_items)</span>";	
+	echo "</h3></div></a>
 		<a href=\"http://localhost:8080/Rush00/src/inscription.php\" style=\"color:white;\"><div id=bloc_menu><h3 style=\"margin-top:25px;\">INSCRIPTION</h3></div></a>
 		<a href=\"http://localhost:8080/Rush00/src/connexion.php\" style=\"color:white;\"><div id=bloc_menu><h3 style=\"margin-top:25px;\">CONNEXION</h3></div></a>";
 	}
 	else {
-		echo "<a href=\"http://localhost:8080/Rush00/src/panier.php\" style=\"color:white;\"><div id=bloc_menu><h3 style=\"margin-top:25px;\">MON PANIER</h3></div></a>
+		echo "<a href=\"http://localhost:8080/Rush00/src/panier.php\" style=\"color:white;\"><div id=bloc_menu><h3 style=\"margin-top:25px;\">MON PANIER";
+	if ($nb_items)
+		echo "<span class='panier_items'> ($nb_items)</span>";	
+	echo "</h3><span class='panier_items'>($nb_items)</span></div></a>
 		<a href=\"http://localhost:8080/Rush00/src/account.php\" style=\"color:white;\"><div id=bloc_menu><h3 style=\"margin-top:25px;\">MON COMPTE</h3></div></a>
 		<a href=\"http://localhost:8080/Rush00/src/logout.php\" style=\"color:white;\"><div id=bloc_menu><h3 style=\"margin-top:25px;\">DECONNEXION</h3></div></a>";
 	}
-	?>
-</div>
+?>
